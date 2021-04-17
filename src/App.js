5ac8eb58cca43d10142dc7f40322f00833cdf1f0
damilare <joeydre69@gmail.com>
2021-04-17 12:59:41 +0200

    hot module reloading, JSX, rendering lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title:"React",
+    url:"",
+    author:"Jordan Walks",
+    num_comments:"3",
+    points:"4",
+    objectID:"0",
+    
+  },
+  {
+    title:"Redux",
+    url:"",
+    author:"Dan Abramov, Andrew Clark",
+    num_comments:"2",
+    points:"5",
+    objectID:"1",
+  }
+]
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        {list.map((item)=>{
+          return (
+            <div key={item.objectID}>
+              <span>
+              <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )
+          
+        })}
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+if (module.hot){
+  module.hot.accept()
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
