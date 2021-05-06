5f391a913ae67e49f2f40f1f8063fe7334ebf1f5
damilare <joeydre69@gmail.com>
2021-05-07 00:12:35 +0200

    replacing native fetch api with axios library and avoid calling this.setState on unmounted components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react'
+import axios from 'axios'
 
 const largeColumn = { width: '40%', };
 const midColumn = { width: '30%', };
@@ -14,7 +15,7 @@ const PARAM_HPP = 'hitsPerPage='
 
 
 class App extends Component {
-
+  _isMounted = false;
   constructor(props) {
     super(props);
     this.state = {
@@ -36,10 +37,9 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => this.setState({error}))
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }))
   }
   onSearchSubmit(e) {
     const { searchTerm } = this.state;
@@ -68,9 +68,13 @@ class App extends Component {
     })
   }
   componentDidMount() {
+    this._isMounted = true;
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm })
     this.fetchSearchTopStories(searchTerm)
+  }
+  componentWillUnmount(){
+    this._isMounted = false;
   }
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value })
@@ -120,12 +124,12 @@ class App extends Component {
         </Search>
         </div>
         {error
-        ? <div className="interactions">
+          ? <div className="interactions">
             <p>Something went wrong</p>
           </div>
-          :<Table
-          list={list}
-          onDismiss={this.onDismiss} />
+          : <Table
+            list={list}
+            onDismiss={this.onDismiss} />
         }
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
